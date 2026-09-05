48c73f9939bb492d96f09d625de7a971c3cf1eda
Bhagyaketha <k.bhagyalakshmi237@gmail.com>
2026-09-05 11:44:55 +0530

    Fix GiftFinder CSS import

--- src/pages/GiftFinder.jsx
+++ src/pages/GiftFinder.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../styles/giftFinder.css";
+import "../styles/giftfinder.css";
 
 const GiftFinder = () => {
 
